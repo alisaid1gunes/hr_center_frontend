1792b51c1191c61f921fb98e9ba76fa3f7cd7f88
alisaidgunes <alisaidgunes1@gmail.com>
2022-07-19 12:56:25 +0300

    create appbar with search

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import SearchAppBar from "../components/SearchAppBar";
 const Main = () => {
     return (
         <div>
+            <SearchAppBar/>
             Main
         </div>
     );
